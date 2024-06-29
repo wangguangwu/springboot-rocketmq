e4c338732c2107eb0e1c6217e18ff9241e947e92
wangguangwu <wangguangwu@pukanghealth.com>
2024-06-29 10:52:35 +0800

    生产者单向消息、批量消息示例

--- client/src/main/java/com/wangguangwu/prodcuer/BatchProducer.java
+++ client/src/main/java/com/wangguangwu/prodcuer/BatchProducer.java
@@ -1,5 +1,15 @@
 package com.wangguangwu.prodcuer;
 
+import com.wangguangwu.common.Constants;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 批量消息生产者
  *
@@ -7,7 +17,29 @@ package com.wangguangwu.prodcuer;
  */
 public class BatchProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        // 配置 producer
+        DefaultMQProducer producer = new DefaultMQProducer("produce-group");
+        producer.setNamesrvAddr(Constants.NAME_SERVER);
+        producer.start();
 
+        List<Message> messageList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            // 设置消息主题、标签
+            Message message = new Message();
+            message.setTopic("batch-topic");
+            message.setKeys("batch-key");
+            message.setTags("batch-tag");
+
+            // 设置消息内容
+            message.setBody("Batch message: hello world".getBytes());
+            messageList.add(message);
+        }
+
+        // 发送消息
+        producer.send(messageList);
+
+        producer.shutdown();
     }
 }
--- client/src/main/java/com/wangguangwu/prodcuer/OneWayProducer.java
+++ client/src/main/java/com/wangguangwu/prodcuer/OneWayProducer.java
@@ -1,5 +1,11 @@
 package com.wangguangwu.prodcuer;
 
+import com.wangguangwu.common.Constants;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
 /**
  * 单向消息生产者
  *
@@ -7,7 +13,24 @@ package com.wangguangwu.prodcuer;
  */
 public class OneWayProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException {
+        // 配置 producer
+        DefaultMQProducer producer = new DefaultMQProducer("produce-group");
+        producer.setNamesrvAddr(Constants.NAME_SERVER);
+        producer.start();
 
+        // 设置消息主题、标签
+        Message message = new Message();
+        message.setTopic("oneway-topic");
+        message.setKeys("oneway-key");
+        message.setTags("oneway-tag");
+
+        // 设置消息内容
+        message.setBody("OneWay message: hello world".getBytes());
+
+        // 发送单向消息
+        producer.sendOneway(message);
+
+        producer.shutdown();
     }
 }
